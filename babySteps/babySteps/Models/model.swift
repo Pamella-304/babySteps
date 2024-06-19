5f74e45ef3e95b77ed6aa9a29816f25ddb6519ed
Pamella <pamella.as.304@gmail.com>
2024-06-19 08:56:15 -0300

    created empty files

---
+++ babySteps/babySteps/Models/model.swift
@@ -0,0 +1,8 @@
+//
+//  model.swift
+//  babySteps
+//
+//  Created by Pamella Alvarenga on 19/06/24.
+//
+
+import Foundation
---
+++ babySteps/babySteps/Utilities/new.swift
@@ -0,0 +1,8 @@
+//
+//  new.swift
+//  babySteps
+//
+//  Created by Pamella Alvarenga on 19/06/24.
+//
+
+import Foundation
