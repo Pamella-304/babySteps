5d66521471e930afcf1d4d9a2526d850f0694fbc
Marcelo P Duarte <marcelopd20@gmail.com>
2024-06-28 23:04:43 -0300

    arrumando MuralView

---
+++ babySteps/MuralView.swift
@@ -0,0 +1,18 @@
+//
+//  MuralView.swift
+//  babySteps
+//
+//  Created by Marcelo Pastana Duarte on 28/06/24.
+//
+
+import SwiftUI
+
+struct MuralView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MuralView()
+}
