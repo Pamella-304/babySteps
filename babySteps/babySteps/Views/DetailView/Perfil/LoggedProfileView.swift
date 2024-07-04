22779c3dd0e3e59f16f52d5e266f43b46dd84c5a
Pamella <pamella.as.304@gmail.com>
2024-07-04 14:21:54 -0300

    dados mockados para contornar o problema da opcional

--- babySteps/babySteps/Views/DetailView/Perfil/LoggedProfileView.swift
+++ babySteps/babySteps/Views/DetailView/Perfil/LoggedProfileView.swift
@@ -161,29 +161,29 @@ struct ClassProfileCardView: View {
         .clipShape(.rect(cornerRadius: 16))
     }
 }
-
-struct CalendarProfileCardView: View {
-    var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            Text("Segunda-Feira")
-                .font(.system(.subheadline, design: .rounded))
-                .foregroundStyle(Color.bluePrimary)
-                .bold()
-            Text("28")
-                .font(.system(size: 36, weight: .medium, design: .rounded))
-            HStack(alignment: .top, spacing: 5) {
-                Capsule()
-                    .frame(maxWidth: 4, maxHeight: .infinity)
-                Text("Reunião de pais e mestres | 14h")
-                    .font(.system(.body , design: .rounded, weight: .regular))
-                
-            }.foregroundStyle(Color.orangePrimary)
-            Text("mais 1 evento")
-                .font(.system(.body , design: .rounded, weight: .regular))
-                .foregroundStyle(Color.gray)
-        }.frame(width: 185, height: 155, alignment: .topLeading)
-            .padding()
-            .background(Color.white)
-            .clipShape(.rect(cornerRadius: 16))
-    }
-}
+//
+//struct CalendarProfileCardView: View {
+//    var body: some View {
+//        VStack(alignment: .leading, spacing: 8) {
+//            Text("Segunda-Feira")
+//                .font(.system(.subheadline, design: .rounded))
+//                .foregroundStyle(Color.bluePrimary)
+//                .bold()
+//            Text("28")
+//                .font(.system(size: 36, weight: .medium, design: .rounded))
+//            HStack(alignment: .top, spacing: 5) {
+//                Capsule()
+//                    .frame(maxWidth: 4, maxHeight: .infinity)
+//                Text("Reunião de pais e mestres | 14h")
+//                    .font(.system(.body , design: .rounded, weight: .regular))
+//                
+//            }.foregroundStyle(Color.orangePrimary)
+//            Text("mais 1 evento")
+//                .font(.system(.body , design: .rounded, weight: .regular))
+//                .foregroundStyle(Color.gray)
+//        }.frame(width: 185, height: 155, alignment: .topLeading)
+//            .padding()
+//            .background(Color.white)
+//            .clipShape(.rect(cornerRadius: 16))
+//    }
+//}
--- babySteps/babySteps/Views/DetailView/Profile/SchoolUserInfoCardView.swift
+++ babySteps/babySteps/Views/DetailView/Profile/SchoolUserInfoCardView.swift
@@ -6,41 +6,41 @@
 //
 
 import SwiftUI
+//
+//struct SchoolUserInfoCardView: View {
+//    let teacher: Teacher
+//    let name: String?
+//    init(teacher: MockData().teacher, name: String? = nil) {
+//        self.teacher = teacher
+//        self.name = teacher.firstName + " " + teacher.lastName
+//    }
+//    var body: some View {
+//        VStack(spacing: 15) {
+//            AsyncImage(url: URL(string: "")) { image in
+//                image
+//                    .resizable()
+//                    .clipShape(Circle())
+//                    .frame(width: 112, height: 112)
+//            } placeholder: {
+//                Image("childPlaceHolder")
+//                    .resizable()
+//                    .clipShape(Circle())
+//                    .frame(width: 112, height: 112)
+//            }
+//
+//            Text(name!)
+//                .font(.system(.title, design: .rounded))
+//                .bold()
+//
+//            Text("\(teacher.email) ")
+//                .font(.system(.title2, design: .rounded))
+//                .fontWeight(.regular)
+//            Spacer()
+//        }
+//    }
+//}
 
-struct SchoolUserInfoCardView: View {
-    let schoolUser: SchoolUser
-    let name: String?
-    init(schoolUser: SchoolUser = MockData().schoolUser, name: String? = nil) {
-        self.schoolUser = schoolUser
-        self.name = schoolUser.firstName + " " + schoolUser.lastName
-    }
-    var body: some View {
-        VStack(spacing: 15) {
-            AsyncImage(url: URL(string: "")) { image in
-                image
-                    .resizable()
-                    .clipShape(Circle())
-                    .frame(width: 112, height: 112)
-            } placeholder: {
-                Image("childPlaceHolder")
-                    .resizable()
-                    .clipShape(Circle())
-                    .frame(width: 112, height: 112)
-            }
 
-            Text(name!)
-                .font(.system(.title, design: .rounded))
-                .bold()
-
-            Text("\(schoolUser.email) ")
-                .font(.system(.title2, design: .rounded))
-                .fontWeight(.regular)
-            Spacer()
-        }
-    }
-}
-
-
-#Preview {
-    SchoolUserInfoCardView()
-}
+//#Preview {
+//    SchoolUserInfoCardView()
+//}
